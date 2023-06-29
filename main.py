050f44c2db6c695de1817d08c85fb02fe9358934
JuanViana <jdviand@gmail.com>
2023-06-28 21:38:27 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+from tensorflow import keras
+
+MODEL_PATH='modelPulsar.h5'
+
+
+# Load Tensorflow model
+model = keras.models.load_model(MODEL_PATH)
+print(model.summary())
+
+
+if len(sys.argv)>0:
+
+    arguments = sys.argv[1:]
+    print(arguments)
+
+    # Process the arguments
+    #for arg in arguments:
+    #    print(arg)
+
+
+    print(model.predict([[float(i) for i in arguments]]))
+
+
+else:
+    print('0 or more than 1 arguments received')
